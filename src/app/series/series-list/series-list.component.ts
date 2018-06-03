56e21969de6501813bb278cc7e32bd7e16358ca4
Rade Krstanovic <radekrstanovic@gmail.com>
2018-06-03 17:02:10 +0200

    Add SeriesList component

---
+++ src/app/series/series-list/series-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+// TODO >>> import models the right way!!!
+import { Series } from '../../shared/models/series.model';
+import { SeriesService } from '../series.service';
+
+@Component({
+  selector: 'app-series-list',
+  templateUrl: './series-list.component.html',
+  styleUrls: ['./series-list.component.css']
+})
+export class SeriesListComponent implements OnInit {
+
+	series: Series[];
+
+  @Input() title: string;
+
+  constructor(private seriesService: SeriesService) { }
+
+  ngOnInit() {
+  	this.getSeries();
+  }
+
+  getSeries() {
+  	this.seriesService.getSeries()
+      .subscribe(series => this.series = series);
+  }
+}
