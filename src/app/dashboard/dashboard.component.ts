0ac8f850d7bf98a6817c9fba04e44c509761f15a
Rade Krstanovic <radekrstanovic@gmail.com>
2018-06-03 17:06:14 +0200

    Add Dashboard component

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+	selectionCriterias = ['recommended', 'newest', 'most popular'];
+	// title = 'recommanded';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
