453aea451f8b1fdee6f4026b090b21c64bd158aa
Rade Krstanovic <radekrstanovic@gmail.com>
2018-06-04 13:52:55 +0200

    Add paths to series list and series detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SeriesListComponent } from './series/series-list/series-list.component';
+import { SeriesDetailComponent } from './series/series-detail/series-detail.component';
 
 const routes: Routes = [
-	{
-		path: '',
-		component: DashboardComponent
-	}
+	{ path: '', component: DashboardComponent },
+	{ path: 'series', component: SeriesListComponent },
+	{ path: 'series/:id', component: SeriesDetailComponent }
 ];
 
 @NgModule({
