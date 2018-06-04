04ee72399a26345b6233b56159cc12f175094c45
Rade Krstanovic <radekrstanovic@gmail.com>
2018-06-04 13:54:29 +0200

    Add SeriesDetailComponent

---
+++ src/app/series/series-detail/series-detail.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Series } from '../../shared/models/series.model';
+import { SeriesService } from '../series.service';
+
+@Component({
+  selector: 'app-series-detail',
+  templateUrl: './series-detail.component.html',
+  styleUrls: ['./series-detail.component.css']
+})
+export class SeriesDetailComponent implements OnInit {
+
+	@Input() series: Series;
+
+  constructor(
+  	private route: ActivatedRoute,
+  	private seriesService: SeriesService,
+  	private location: Location
+  ) {}
+
+  ngOnInit(): void {
+  	this.getSeries();
+  }
+
+  getSeries() {
+  	const seriesId = +this.route.snapshot.paramMap.get('id');
+  	this.seriesService.getSeriesById(seriesId)
+  		.subscribe(series => this.series = series);
+  }
+
+}
