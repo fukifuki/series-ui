d9156ce8930b9178c73846d596c18f6d8ab223e0
Rade Krstanovic <radekrstanovic@gmail.com>
2018-06-10 17:16:02 +0200

    Add constructor to Series model

--- src/app/shared/models/series.model.ts
+++ src/app/shared/models/series.model.ts
@@ -1,7 +1,13 @@
 export class Series {
+	
 	id: number;
 	title: string;
 	description: string;
 	yearStarted: number;
 	genreName: string;
+
+	constructor(values: Object = {}) {
+		Object.assign(this, values);
+	}
+
 }
