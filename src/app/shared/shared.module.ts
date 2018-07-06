3365495469acccd1aa83e980f2f705ee9975bbb3
Rade Krstanovic <radekrstanovic@gmail.com>
2018-07-06 11:57:35 +0200

    Add header that provides user with sign in/sign out buttons and link to access their profile page

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from '../app-routing.module';
 import { SearchComponent } from './search/search.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AppRoutingModule
   ],
-  declarations: [SearchComponent],
-  exports: [SearchComponent]
+  declarations: [SearchComponent, HeaderComponent],
+  exports: [SearchComponent, HeaderComponent]
 })
 export class SharedModule { }
